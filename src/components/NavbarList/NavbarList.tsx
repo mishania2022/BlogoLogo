30bada1049a0be1cf8c81170315c43de370e3fd0
mikhail <VO3@Pgasn>
2022-10-08 11:29:29 +0300

    refactor: fix bugs

--- src/components/NavbarList/NavbarList.tsx
+++ src/components/NavbarList/NavbarList.tsx
@@ -20,8 +20,8 @@ export const NavbarList = ({ open }: IProps) => {
           <FavoritesArticles />
           <FavoritesBlogs />
           <Badge
-            userName={name[0] ? name[0] : name}
-            userSurname={surname[0] ? surname[0] : surname}
+            userName={name === "" ? name : name[0]}
+            userSurname={surname === "" ? surname : surname[0]}
           />
         </>
       ) : (
